4161a78bd1e9dcda4bc34a77473d824f3c1ada64
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-12-14 12:21:35 +0530

    task: Implemented Resume Preview Page

--- 01 - AI Resume Builder/frontend/src/pages/Preview.jsx
+++ 01 - AI Resume Builder/frontend/src/pages/Preview.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useDeferredValue, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import ResumePreview from "../components/ResumePreview";
+import Loader from "../components/Loader";
+import { ArrowLeftIcon } from "lucide-react";
 
 const Preview = () => {
-  return <div>Preview</div>;
+  const { resumeId } = useParams();
+  const [resumeData, setResumeData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Function to load resume data
+  const loadResume = async () => {
+    setResumeData(
+      dummyResumeData.find((resume) => resume._id === resumeId || null)
+    );
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadResume();
+  }, []);
+
+  return resumeData ? (
+    <div className="bg-neutral-100">
+      <div className="max-w-3xl mx-auto py-10">
+        <ResumePreview
+          classes="py-4 bg-white"
+          data={resumeData}
+          template={resumeData.template}
+          accentColor={resumeData.accent_color}
+        />
+      </div>
+    </div>
+  ) : (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <p className="text-center text-6xl text-red-600 font-medium">
+            Resume Not Found
+          </p>
+          <a
+            href="/"
+            className="mt-6 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-indigo-400 flex items-center transition-colors"
+          >
+            <ArrowLeftIcon className="size-4 mr-2" />
+            Go to Home Page
+          </a>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Preview;
