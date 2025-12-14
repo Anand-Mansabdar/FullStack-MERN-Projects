862c845a06e6da46646c20af4d6a0a40cd3a8a01
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-12-14 10:44:58 +0530

    task: Implemented "Skills" Component

--- 01 - AI Resume Builder/frontend/src/pages/ResumeBuilder.jsx
+++ 01 - AI Resume Builder/frontend/src/pages/ResumeBuilder.jsx
@@ -20,6 +20,7 @@ import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -218,7 +219,21 @@ const ResumeBuilder = () => {
                     }
                   />
                 )}
+
+                {activeSection.id === "skills" && (
+                  <SkillsForm
+                    data={resumeData.skills}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        skills: data,
+                      }))
+                    }
+                  />
+                )}
               </div>
+
+              
             </div>
           </div>
 
